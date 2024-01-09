07b054e86c47eceebbc8084dac351f92045e472c
Thomas <thomashess1012@gmail.com>
2024-01-09 13:11:57 -0300

    Testimonial done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <Services />
+      <Testimonial />
     </>
   );
 }
---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,241 @@
+import React from "react";
+
+export default function Testimonial() {
+  return (
+    <section className="py-10 md:py-16 bg-neutral">
+      <div className="container">
+        <div className="text-center text-white">
+          <h2 className="text-3xl sm:text-5xl font-bold mb-4">
+            We have some fans.
+          </h2>
+          <p className="text-lg sm:text-2xl mb-6 md:mb-14">
+            We create engaging experiences that are innovating and beautiful.
+          </p>
+        </div>
+
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-6 lg:gap-8">
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/100/100" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        Eric Ampire
+                      </p>
+                      <small>Mobile Dev</small>
+                    </div>
+                  </div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/seed/people/100/100" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        John Wayne
+                      </p>
+                      <small>Manager</small>
+                    </div>
+                  </div>
+                  <p>
+                    Nulla purus mauris, interdum ac convallis quis, tempus ac
+                    massa. Fusce a venenatis dui. Mauris in lorem egestas,
+                    pulvinar mauris vitae, laoreet justo. Donec a erat quis nisi
+                    iaculis porttitor. Aliquam quis tortor et lacus aliquet
+                    lacinia. Curabitur vitae iaculis nisi, eget interdum urna.
+                    Sed ac risus rhoncus, feugiat libero ac, faucibus dolor.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/seed/man/101/101" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        Andrew Bishop
+                      </p>
+                      <small>Product Owner</small>
+                    </div>
+                  </div>
+                  <p>
+                    Aliquam erat volutpat. Integer id sapien rutrum, efficitur
+                    risus non, dictum diam. Aliquam id vestibulum libero. Nulla
+                    sollicitudin rutrum lacinia. Praesent eu dapibus lectus,
+                    vitae tempus diam. Nulla facilisi. Maecenas sit amet magna
+                    commodo, tristique nulla vitae, porta magna. Nunc id quam at
+                    dui finibus dapibus. Pellentesque vitae egestas arcu, id
+                    rhoncus justo. Cras pretium massa et nisl ultrices auctor.
+                    Praesent id facilisis odio, sit amet interdum lorem. Vivamus
+                    rhoncus molestie massa, in semper metus venenatis ut. Nulla
+                    consequat purus a augue aliquam, at scelerisque sem
+                    eleifend. Nullam tristique, lorem vitae pulvinar egestas,
+                    libero metus dignissim elit, sed mattis justo ipsum at
+                    velit.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/seed/man1/99/99" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        Robert P. William
+                      </p>
+                      <small>Senior Web Dev</small>
+                    </div>
+                  </div>
+                  <p>
+                    Proin tincidunt diam id posuere blandit. Mauris quis justo
+                    vulputate, varius ipsum vel, posuere eros. Duis nec lacus
+                    sit amet magna tempor semper quis a eros. Pellentesque
+                    ornare ornare turpis sed tristique. Vivamus non libero
+                    tortor. Nullam porttitor dictum dictum. Nulla suscipit
+                    mauris vel risus elementum mattis. Phasellus imperdiet velit
+                    ac urna vestibulum egestas. Praesent porta velit suscipit
+                    nisl feugiat, nec lacinia ligula semper. Duis non sodales
+                    risus, id tristique tellus. Curabitur porttitor risus eget
+                    velit sodales ultrices.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/seed/woman/100/100" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        Victoria Smith
+                      </p>
+                      <small>Designer</small>
+                    </div>
+                  </div>
+                  <p>
+                    Nam sollicitudin fringilla hendrerit. Duis eget libero
+                    tempus, dapibus urna id, euismod ligula. Pellentesque
+                    ultricies pretium feugiat. Quisque felis dui, aliquet nec mi
+                    at, eleifend efficitur enim. Aliquam sed lorem quis lectus
+                    semper feugiat sit amet ac ligula. Morbi vitae interdum
+                    dolor. Pellentesque habitant morbi tristique senectus et
+                    netus et malesuada fames ac turpis egestas. Etiam vel diam
+                    et orci tristique vulputate. Maecenas lacinia nec neque vel
+                    tristique. Nullam felis ante, hendrerit porttitor lacus sed,
+                    auctor venenatis nisi. Suspendisse consectetur lorem a
+                    tempus faucibus. Integer aliquam semper tellus. In viverra
+                    vel mauris sed faucibus. Suspendisse potenti.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+
+          <div className="block mb-8 lg:mb-10 ">
+            <div className="stack ">
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className="flex items-center">
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://picsum.photos/seed/men/100/100" />
+                      </div>
+                    </div>
+
+                    <div className="ml-3 ">
+                      <p className="text-lg font-medium leading-tight">
+                        Ronald Mcdonald
+                      </p>
+                      <small>Franchise Owner</small>
+                    </div>
+                  </div>
+                  <p>
+                    Sed finibus sapien ac semper tincidunt. In euismod, nulla id
+                    elementum blandit, odio purus viverra augue, quis varius
+                    diam nisi semper mauris. Donec id lacus mauris. Aliquam
+                    accumsan lorem in velit efficitur, elementum sodales nulla
+                    rhoncus. Cras ut mauris sit amet erat iaculis ultricies.
+                    Aenean condimentum luctus justo, in auctor ex elementum sit
+                    amet. Nullam mi nulla, pharetra vitae mauris non, fringilla
+                    aliquet mauris. Maecenas eget elementum mauris. Morbi rutrum
+                    eros orci, a fringilla mauris tempor non. Mauris a lorem
+                    turpis.
+                  </p>
+                </div>
+              </div>
+              <div className="card bg-primary h-10"></div>
+              <div className="card bg-warning h-10"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
