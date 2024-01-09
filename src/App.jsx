206e5c6e1a6817b20e8cb39e2f417299a99d975e
Thomas <thomashess1012@gmail.com>
2024-01-09 11:39:07 -0300

    services component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
     </>
   );
 }
